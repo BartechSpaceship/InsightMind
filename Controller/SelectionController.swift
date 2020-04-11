12932616fdc454fc92187e21bff63c30e53769d2
Bartek <bartek.daniel24@gmail.com>
2020-04-11 17:45:54 -0500

    UI & Timer Location
    Added the main screen + 5 buttons, duration in the timer section and a new start button displaying time timer

---
+++ Controller/SelectionController.swift
@@ -0,0 +1,26 @@
+//
+//  SelectionController.swift
+//  InsightMind
+//
+//  Created by Bartek on 4/11/20.
+//  Copyright © 2020 Bartek. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class SelectionController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    @IBAction func startButton(_ sender: Any) {
+        print("Start Button Pressed")
+    }
+    
+    
+    @IBAction func durationButton(_ sender: UIButton) {
+        print("Select Duration")
+    }
+    
+}
